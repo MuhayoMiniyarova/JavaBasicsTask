3602062053408a89021e6285b6657cbdcf2f7396
MuhayoMiniyarova <muhayominiyarova@gmail.com>
2023-08-21 17:26:23 -0400

    message

--- Task1.java
+++ Task1.java
@@ -3,21 +3,26 @@ package CodingTasks;
 public class Task1 {
     public static void main(String[] args) {
 
-        int [] temp={67,78,90,102,37,81};
+        int [] temp={67,78,90,102,37,81,40};
         int highest=temp[0];
         int lowest=temp[0];
         for (int i = 0; i <temp.length ; i++) {
-            if (i>highest) {
-                System.out.println("The highest temperature of the week is "+highest);
-            } else if (i<lowest) {
+            if (temp[i] > highest) {
+                highest = temp[i];
+            }
+            if (temp[i] < lowest) {
+                lowest = temp[i];
+            }
+        }
+        System.out.println("The highest temperature of the week is "+highest);
                 System.out.println("The lowest temperature of the week is "+lowest);
 
             }
         }
 
-        }
 
 
 
-    }
+
+
 
